42d600b7c44ca751556e270fe5437e0b3227408f
lautarojaguilera-del <lautaroj.aguilera@gmail.com>
2026-04-04 19:07:16 -0300

    feat: Implement automated checkout testing
    Introduces a Puppeteer script to automate the checkout process for the FoxMoto store. This includes adding items to the cart, proceeding to checkout, and filling in customer and address details. Also adds necessary configuration files and updates README.

--- list_zip.cjs
+++
@@ -1,6 +0,0 @@
-const AdmZip = require('adm-zip');
-const zip = new AdmZip('foxmoto-store.zip');
-const zipEntries = zip.getEntries();
-zipEntries.forEach(function(zipEntry) {
-    console.log(zipEntry.entryName);
-});
